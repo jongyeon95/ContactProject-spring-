b08250e660ee5aa759eb64ec48c433715056589b
whddus9509 <whddus9509@naver.com>
2020-03-05 19:54:58 +0900

    Update PersonApiController.java
    not yet

--- src/main/java/kr/co/contactproject/contact/api/controller/PersonApiController.java
+++ src/main/java/kr/co/contactproject/contact/api/controller/PersonApiController.java
@@ -1,8 +1,10 @@
 package kr.co.contactproject.contact.api.controller;
 
-import kr.co.contactproject.contact.ifs.CrudInterface;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PersonApiController {
+@RequestMapping("/api/person")
+public class PersonApiController{
+
 }
