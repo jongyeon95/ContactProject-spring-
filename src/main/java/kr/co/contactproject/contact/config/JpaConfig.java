65ed713722e60c61166a9056bf8147195a4a728f
whddus9509 <whddus9509@naver.com>
2020-03-05 18:11:27 +0900

    Create JpaConfig.java

---
+++ src/main/java/kr/co/contactproject/contact/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package kr.co.contactproject.contact.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA 감시 활성화
+public class JpaConfig {
+
+}
