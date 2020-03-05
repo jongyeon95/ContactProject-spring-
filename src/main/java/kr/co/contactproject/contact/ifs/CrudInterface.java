6bed0921cf0325b7636e57dac2abd3c8b3105c8a
whddus9509 <whddus9509@naver.com>
2020-03-05 19:49:00 +0900

    Update CrudInterface.java

--- src/main/java/kr/co/contactproject/contact/ifs/CrudInterface.java
+++ src/main/java/kr/co/contactproject/contact/ifs/CrudInterface.java
@@ -1,5 +1,15 @@
 package kr.co.contactproject.contact.ifs;
 
-public interface CrudInterface {
+import kr.co.contactproject.contact.model.network.Header;
+
+public interface CrudInterface<Req, Res> {
+
+    Header<Res> create(Header<Req> request);
+
+    Header<Res> read(Long id);
+
+    Header<Res> update(Header<Req> request);
+
+    Header delete(Long id);
 
 }
