7bb6f5358ab788066e49b2e18d07a4a7f02f10a8
whddus9509 <whddus9509@naver.com>
2020-03-05 19:48:44 +0900

    Create PersonApiController.java

---
+++ src/main/java/kr/co/contactproject/contact/api/controller/PersonApiController.java
@@ -0,0 +1,8 @@
+package kr.co.contactproject.contact.api.controller;
+
+import kr.co.contactproject.contact.ifs.CrudInterface;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonApiController {
+}
