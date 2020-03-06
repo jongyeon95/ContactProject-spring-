cd9903efa8bdc2b4e48ffb0e0a5a0f067308177e
whddus9509 <whddus9509@naver.com>
2020-03-06 19:41:36 +0900

    upadte entity
    add @EntityListeners

--- src/main/java/kr/co/contactproject/contact/model/entity/GroupDetail.java
+++ src/main/java/kr/co/contactproject/contact/model/entity/GroupDetail.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,6 +18,8 @@ import java.time.LocalDateTime;
 @Builder
 @Accessors(chain = true)
 @ToString(exclude = {"person"})
+@EntityListeners(AuditingEntityListener.class)
+
 public class GroupDetail {
 
 
--- src/main/java/kr/co/contactproject/contact/model/entity/Person.java
+++ src/main/java/kr/co/contactproject/contact/model/entity/Person.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -19,6 +20,8 @@ import java.util.List;
 @Builder
 @Accessors(chain = true)
 @ToString(exclude = {"GroupDetail"})
+@EntityListeners(AuditingEntityListener.class)
+
 public class Person {
 
     @Id
