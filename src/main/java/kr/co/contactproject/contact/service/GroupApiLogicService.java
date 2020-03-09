a865fb81a725c3ee92ba3fce09aa989d8a8be89c
whddus9509 <whddus9509@naver.com>
2020-03-09 18:24:47 +0900

    Create GroupApiLogicService.java

---
+++ src/main/java/kr/co/contactproject/contact/service/GroupApiLogicService.java
@@ -0,0 +1,50 @@
+package kr.co.contactproject.contact.service;
+
+import kr.co.contactproject.contact.model.entity.Group;
+import kr.co.contactproject.contact.model.network.Header;
+import kr.co.contactproject.contact.model.network.request.GroupApiRequest;
+import kr.co.contactproject.contact.model.network.response.GroupApiResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupApiLogicService extends BaseService<GroupApiRequest, GroupApiResponse, Group> {
+    @Override
+    public Header<GroupApiResponse> create(Header<GroupApiRequest> request) {
+
+        GroupApiRequest body=request.getData();
+        Group group=Group.builder()
+                .name(body.getName())
+                .groupDetail(body.getGroupDetail())
+                .description(body.getDescription())
+                .build();
+
+        return null;
+    }
+
+    @Override
+    public Header<GroupApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<GroupApiResponse> update(Header<GroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<GroupApiResponse> response(Group group){
+
+        GroupApiResponse groupApiResponse=GroupApiResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getName())
+                .groupDetail(group.getGroupDetail())
+                .build();
+
+        return Header.OK(groupApiResponse);
+    }
+}
