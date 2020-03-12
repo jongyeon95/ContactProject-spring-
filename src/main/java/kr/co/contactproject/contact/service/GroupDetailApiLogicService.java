15e1abbdb4ce29289462910b29501c6a14a91669
whddus9509 <whddus9509@naver.com>
2020-03-13 05:12:15 +0900

    Update GroupDetailApiLogicService.java
    Reformat code

--- src/main/java/kr/co/contactproject/contact/service/GroupDetailApiLogicService.java
+++ src/main/java/kr/co/contactproject/contact/service/GroupDetailApiLogicService.java
@@ -1,10 +1,7 @@
 package kr.co.contactproject.contact.service;
 
-import kr.co.contactproject.contact.model.entity.Group;
 import kr.co.contactproject.contact.model.entity.GroupDetail;
-import kr.co.contactproject.contact.model.entity.Person;
 import kr.co.contactproject.contact.model.network.Header;
-import kr.co.contactproject.contact.model.network.request.GroupApiRequest;
 import kr.co.contactproject.contact.model.network.request.GroupDetailApiRequest;
 import kr.co.contactproject.contact.model.network.response.GroupDetailApiResponse;
 import kr.co.contactproject.contact.respository.GroupRepository;
@@ -12,7 +9,6 @@ import kr.co.contactproject.contact.respository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class GroupDetailApiLogicService
@@ -21,17 +17,16 @@ public class GroupDetailApiLogicService
     private PersonRepository personRepository;
     @Autowired
     private GroupRepository groupRepository;
+
     @Override
     public Header<GroupDetailApiResponse> create(Header<GroupDetailApiRequest> request) {
-        GroupDetailApiRequest body=request.getData();
-        GroupDetail groupDetail=GroupDetail.builder()
+        GroupDetailApiRequest body = request.getData();
+        GroupDetail groupDetail = GroupDetail.builder()
                 .group(groupRepository.getOne(body.getGroupId()))
                 .person(personRepository.getOne(body.getPersonId()))
                 .build();
-        GroupDetail newGroupDetail=baseRepository.save(groupDetail);
+        GroupDetail newGroupDetail = baseRepository.save(groupDetail);
         return response(newGroupDetail);
-
-
 
 
     }
@@ -40,44 +35,45 @@ public class GroupDetailApiLogicService
     public Header<GroupDetailApiResponse> read(Long id) {
 
         return baseRepository.findById(id).map(groupDetail -> {
-            GroupDetail newGroupDetail= GroupDetail.builder()
-                    .id(groupDetail.getId())
-                    .group(groupDetail.getGroup())
-                    .person(groupDetail.getPerson())
-                    .build();
-            return response(newGroupDetail);
-        }
-        ).orElseGet(()->Header.ERROR("데이터 없음"));
+                    GroupDetail newGroupDetail = GroupDetail.builder()
+                            .id(groupDetail.getId())
+                            .group(groupDetail.getGroup())
+                            .person(groupDetail.getPerson())
+                            .build();
+                    return response(newGroupDetail);
+                }
+        ).orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
     public Header<GroupDetailApiResponse> update(Header<GroupDetailApiRequest> request) {
-        GroupDetailApiRequest body=request.getData();
+        GroupDetailApiRequest body = request.getData();
         return baseRepository.findById(body.getId()).map(
-                gd ->{
+                gd -> {
                     gd.setId(body.getId());
                     gd.setGroup(groupRepository.getOne(body.getGroupId()));
                     gd.setPerson(personRepository.getOne(body.getPersonId()));
-                    GroupDetail newGd=baseRepository.save(gd);
+                    GroupDetail newGd = baseRepository.save(gd);
                     return newGd;
                 }
-        ).map(newGd->response(newGd))
-                .orElseGet(()->Header.ERROR("데이터없음"));
+        ).map(newGd -> response(newGd))
+                .orElseGet(() -> Header.ERROR("데이터없음"));
 
     }
 
     @Override
     public Header delete(Long id) {
         return baseRepository.findById(id).map(
-                groupDetail -> {baseRepository.delete(groupDetail);
-                return Header.OK();
+                groupDetail -> {
+                    baseRepository.delete(groupDetail);
+                    return Header.OK();
                 }
-        ).orElseGet(()->Header.ERROR("데이터없음"));
+        ).orElseGet(() -> Header.ERROR("데이터없음"));
 
     }
 
-    public Header<GroupDetailApiResponse> response(GroupDetail groupDetail){
-        GroupDetailApiResponse groupDetailApiResponse=GroupDetailApiResponse.builder()
+    public Header<GroupDetailApiResponse> response(GroupDetail groupDetail) {
+        GroupDetailApiResponse groupDetailApiResponse = GroupDetailApiResponse.builder()
                 .id(groupDetail.getId())
                 .groupName(groupDetail.getGroup().getName())
                 .personName(groupDetail.getPerson().getName())
